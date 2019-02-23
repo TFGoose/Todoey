51867f7d3d9087a57736e5560c6d1f01d1a368af
Rich Rouse <rich.rouse@gmail.com>
2019-02-23 18:28:31 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Richard Rouse on 2/23/19.
+//  Copyright © 2019 Rich Rouse. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title:String = ""
+    var done:Bool = false
+}
