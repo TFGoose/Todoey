b5ac6203ac3142fd2f22e938df0b18185dec7163
Rich Rouse <rich.rouse@gmail.com>
2019-02-24 11:52:25 -0500

    Created empty Category View Controller

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Richard Rouse on 2/24/19.
+//  Copyright © 2019 Rich Rouse. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    let categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    //MARK: - TableViewDatasource Methods
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    
+    //MARK: - Add New Categories
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+   
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+    
+}
